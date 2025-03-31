696ac849666e410f9c79826edaef78c8a92296fb
AnnahRandria <niniannah@gmail.com>
2025-03-31 15:11:32 +0200

    modification contenu

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 export const modalContent = `
   <h2>Options d'accès à l'Approche de Test</h2>
     <ul>
-        <li><a class "text" href="https://annahrandriami.github.io/Mon_portfolio/document/Approche test test sur le jeu Tetris.pdf" target="_blank">Télécharger le PDF</a></li>
-        <li><a href="https://trello.com/b/3HhyIjkA" target="_blank">Voir sur Trello</a></li>
+        <li><a class "text" href="https://github.com/AnnahRandriami/Mon_portfolio/issues" target="_blank">Bug tracking sur github</a></li>
+        <li><a href="https://trello.com/b/3HhyIjkA" target="_blank">Trello</a></li>
     </ul>
 `;
