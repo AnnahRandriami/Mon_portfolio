43866f63ea734fed23fdd2b4cd16508f4505771b
AnnahRandria <niniannah@gmail.com>
2025-04-01 14:15:42 +0200

    ajout image ecran plus petit

--- js/demo.js
+++ js/demo.js
@@ -3,11 +3,11 @@ export const modalContent = `
     <ul>
         <li><a class "text" href="https://github.com/AnnahRandriami/Mon_portfolio/issues" target="_blank">Test statique du jeu tetris avec eslint</a></li>
         <li><a href="https://trello.com/b/3HhyIjkA" target="_blank">Test de fonctionalité du jeu tetris en selenium</a></li>
-        <li><a href="https://trello.com/b/3HhyIjkA" target="_blank">Test de fonctionalité de l'estimation de l'effort de test en selenium</a></li>
+        <li><a onclick="window.location.href='./pages_html/demo1.html'">Test de fonctionalité de l'estimation de l'effort de test en selenium</a></li>
     </ul>
       <h2 class="entete">Test des fonctionnalité du site shopify </h2>
     <ul>
 
-        <li><a href="https://trello.com/b/3HhyIjkA" target="_blank">Test de fonctionalité connexion/inscription sur pytest</a></li>
+        <li><a href="https://trello.com/b/3HhyIjkA" target="_blank">Test de fonctionalité connexion/inscription sur selenium</a></li>
     </ul>
 `;
